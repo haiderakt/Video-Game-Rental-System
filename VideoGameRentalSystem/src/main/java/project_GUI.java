d0fc2da4453e2e65b10c4be32f5996bd9a8b9446
Haider Ali <haiderakt@gmail.com>
2023-12-04 20:35:44 +0500

    4-12-2023
    Displayed Staff and Customer data on the main page

--- VideoGameRentalSystem/src/main/java/project_GUI.java
+++ VideoGameRentalSystem/src/main/java/project_GUI.java
@@ -3,6 +3,8 @@
 //FA22-BCS-135 Mohammad Faizan
 //Project Title: Video Game Rental System
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -14,28 +16,30 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class project_GUI extends JFrame{
+
+    JPanel pnlMainMenu = new JPanel();
+
+    
     
     JMenuBar menuBar = new JMenuBar();
     JMenu mainMenu = new JMenu("Main");
     JMenu gameMenu = new JMenu("Game");
     JMenu staffMenu = new JMenu("Staff");
     JMenu customerMenu = new JMenu("Customer");
-    JMenu transactionMenu = new JMenu("Transaction");
-    
+    JMenu transactionMenu = new JMenu("Transaction");    
     JMenuItem itemAddGame = new JMenuItem("Add Game");
     JMenuItem itemUpdateGame = new JMenuItem("Update Game");
     
-    JPanel container = new JPanel();
-    JPanel pnl1 = new JPanel();
-    JPanel pnlAddGame = new JPanel();
     
+
+    JLabel lblTotalGame = new JLabel("Total Video Games: " + (Main.gameCounter + 1));
+    JLabel lblTotalStaff = new JLabel("Total Staff Members: " + (Main.staffCounter + 1));
+    JLabel lblTotalCustomer = new JLabel("Total Registered Customers: " + (Main.customerCounter + 1));
+    JLabel lblCompanyName = new JLabel("GameFlix");
     
-    
-    JButton b1 = new JButton("Video Game menu");
-    JButton b2 = new JButton("Customer menu");
-    JButton b3 = new JButton("Staff menu");
-    JButton b4 = new JButton("Record a transaction");
-    JButton bExit = new JButton("Exit the program");
+    Color colorBlue = new Color(10,11,56);
+    Color colorWhite = new Color(255,255,255);
+
     
     
     public project_GUI(){
@@ -44,18 +48,46 @@ public class project_GUI extends JFrame{
         super.setSize(500, 480);
         super.setResizable(false);
         super.setLayout(null);
-       
-       
-       
-        pnl1.setLayout(null);
-        pnl1.setSize(500, 400);
-        pnlAddGame.setLayout(null);
-        pnlAddGame.setSize(500,400);
-        container.setLayout(null);
-        container.setSize(500,400);
-        container.setVisible(true);
+        super.setVisible(true);
+        
+        
+        
+        gameMenu.add(itemAddGame);
+        gameMenu.add(itemUpdateGame);
+        menuBar.add(mainMenu);
+        menuBar.add(gameMenu);
+        menuBar.add(staffMenu);
+        menuBar.add(customerMenu);
+        menuBar.add(transactionMenu);
+        
        
         
+        lblTotalGame.setBounds(100,170,300,40);
+        lblTotalGame.setForeground(colorWhite);
+        lblTotalStaff.setBounds(100,220,300,40);
+        lblTotalStaff.setForeground(colorWhite);
+        lblTotalCustomer.setBounds(100,270,300,40);
+        lblTotalCustomer.setForeground(colorWhite);
+        lblCompanyName.setFont(new Font("Calibri", 0, 42));
+        lblCompanyName.setBounds(165,40,300,50);
+        lblCompanyName.setForeground(colorWhite);
+
+        
+        
+    
+        pnlMainMenu.setLayout(null);
+        pnlMainMenu.setVisible(true);
+        pnlMainMenu.setSize(500,480);
+        pnlMainMenu.setBackground(colorBlue);
+        pnlMainMenu.add(lblTotalGame);
+        pnlMainMenu.add(lblTotalStaff);
+        pnlMainMenu.add(lblTotalCustomer);
+        pnlMainMenu.add(lblCompanyName);
+        super.setJMenuBar(menuBar);
+        super.add(pnlMainMenu);
+        
+        
+
         
         
         
@@ -63,83 +95,14 @@ public class project_GUI extends JFrame{
         itemAddGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
-            
-            
-        
+                // panel to add video game
            }
        });
        
        
        
        
-       gameMenu.add(itemAddGame);
-       gameMenu.add(itemUpdateGame);
        
-       
-       menuBar.add(mainMenu);
-       menuBar.add(gameMenu);
-       menuBar.add(staffMenu);
-       menuBar.add(customerMenu);
-       menuBar.add(transactionMenu);
-       super.setJMenuBar(menuBar);
-       
-       
-       
-       
-       JLabel lbl1 = new JLabel("Main Menu:\n");
-       lbl1.setBounds(199, 20, 280, 45);
-       
-       
-       b1.setBounds(90, 80, 280, 45);
-       b2.setBounds(90, 140, 280, 45);
-       b3.setBounds(90, 200, 280, 45);
-       b4.setBounds(90, 260, 280, 45);
-       bExit.setBounds(90, 320, 280, 45);       
-       
-       
-       pnl1.add(lbl1);
-       pnl1.add(b1);
-       pnl1.add(b2);
-       pnl1.add(b3);
-       pnl1.add(b4);
-       pnl1.add(bExit);
-        
-       
-       
-       
-       
-//       super.add(pnl1);
-//       pnl1.setVisible(true);
-//       super.add(pnlAddGame);
-//       pnlAddGame.setVisible(true);
-
-
-        container.add(pnl1);
-        super.add(container);
-        super.setVisible(true);
-
-        
-        
-       // Video Game Menu call action.
-       b1.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               
-               
-           
-           }
-       });
-        
-        
-        // Program exit action.
-        bExit.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               
-               System.exit(0);
-           }
-       });
         
         
         
@@ -152,10 +115,6 @@ public class project_GUI extends JFrame{
     
     
     
-//    public static void main(String[] args) {
-//         project_GUI p = new project_GUI();
-//         p.main_GUI();
-//    }}
 
 
     
