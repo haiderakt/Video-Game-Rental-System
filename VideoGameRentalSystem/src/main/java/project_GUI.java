638fe7df4b0e8c13fd298ef38cce7c12dfa05772
Haider Ali <haiderakt@gmail.com>
2023-12-05 22:17:54 +0500

    5-12-2023
    project_GUI pe error fixed

--- VideoGameRentalSystem/src/main/java/project_GUI.java
+++ VideoGameRentalSystem/src/main/java/project_GUI.java
@@ -16,107 +16,171 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class project_GUI extends JFrame{
-
-    JPanel pnlMainMenu = new JPanel();
-
+    // Panels:
+    JPanel panelStats = new JPanel();
+    JPanel panelAddGame = new JPanel();
+    JPanel panelUpdateGame = new JPanel();
     
     
+    
+    // Menu and related:
     JMenuBar menuBar = new JMenuBar();
-    JMenu mainMenu = new JMenu("Main");
-    JMenu gameMenu = new JMenu("Game");
-    JMenu staffMenu = new JMenu("Staff");
-    JMenu customerMenu = new JMenu("Customer");
-    JMenu transactionMenu = new JMenu("Transaction");    
+    JMenu menuMain = new JMenu("Main");
+    JMenu menuGame = new JMenu("Game");
+    JMenu menuStaff = new JMenu("Staff");
+    JMenu menuCustomer =  new JMenu("Customer");
+    JMenu menuTransaction = new JMenu("Transaction");    
     JMenuItem itemAddGame = new JMenuItem("Add Game");
     JMenuItem itemUpdateGame = new JMenuItem("Update Game");
+    JMenuItem itemStats = new JMenuItem("Stats");
     
-    
-
+    // Labels:
     JLabel lblTotalGame = new JLabel("Total Video Games: " + (Main.gameCounter + 1));
     JLabel lblTotalStaff = new JLabel("Total Staff Members: " + (Main.staffCounter + 1));
     JLabel lblTotalCustomer = new JLabel("Total Registered Customers: " + (Main.customerCounter + 1));
     JLabel lblCompanyName = new JLabel("GameFlix");
+    JLabel lblProjectName = new JLabel("Video Game Rental System");
+    JLabel lblAddGameMenu = new JLabel("Add a Video Game");
+    JLabel lblUpdateGameMenu = new JLabel("Update a Video Game");
     
-    Color colorBlue = new Color(10,11,56);
+    // Fonts and Colors:
+    Font fontSubtext = new Font("Calibri", 0, 18);
+    Color colorVeryBlue = new Color(24, 29, 49);
     Color colorWhite = new Color(255,255,255);
-
     
     
-    public project_GUI(){
+    public project_GUI{
+        // JFrame settings:
         super.setTitle("Video Game Rental");
         super.setDefaultCloseOperation(3);
         super.setSize(500, 480);
         super.setResizable(false);
         super.setLayout(null);
-        super.setVisible(true);
         
         
+        // MenuBar settings:
+        menuMain.add(itemStats);
         
-        gameMenu.add(itemAddGame);
-        gameMenu.add(itemUpdateGame);
-        menuBar.add(mainMenu);
-        menuBar.add(gameMenu);
-        menuBar.add(staffMenu);
-        menuBar.add(customerMenu);
-        menuBar.add(transactionMenu);
+        menuGame.add(itemAddGame);
+        menuGame.add(itemUpdateGame);
         
-       
+        menuBar.add(menuMain);
+        menuBar.add(menuGame);
+        menuBar.add(menuStaff);
+        menuBar.add(menuCustomer);
+        menuBar.add(menuTransaction);
+        menuBar.setVisible(true);
         
-        lblTotalGame.setBounds(100,170,300,40);
+        
+        // Components' settings:
+        lblTotalGame.setBounds(100,195,300,40);
         lblTotalGame.setForeground(colorWhite);
-        lblTotalStaff.setBounds(100,220,300,40);
+        
+        lblTotalStaff.setBounds(100,245,300,40);
         lblTotalStaff.setForeground(colorWhite);
-        lblTotalCustomer.setBounds(100,270,300,40);
+        
+        lblTotalCustomer.setBounds(100,295,300,40);
         lblTotalCustomer.setForeground(colorWhite);
+        
         lblCompanyName.setFont(new Font("Calibri", 0, 42));
-        lblCompanyName.setBounds(165,40,300,50);
+        lblCompanyName.setBounds(165,55,300,50);
         lblCompanyName.setForeground(colorWhite);
-
+        
+        lblProjectName.setFont(fontSubtext);
+        lblProjectName.setBounds(146,85,300,50);
+        lblProjectName.setForeground(colorWhite);
+        
+        lblAddGameMenu.setFont(fontSubtext);
+        lblAddGameMenu.setBounds(180,30,300,50);
+        lblAddGameMenu.setForeground(colorWhite);
+        
+        lblUpdateGameMenu.setFont(fontSubtext);
+        lblUpdateGameMenu.setBounds(165,30,300,50);
+        lblUpdateGameMenu.setForeground(colorWhite);        
         
         
-    
-        pnlMainMenu.setLayout(null);
-        pnlMainMenu.setVisible(true);
-        pnlMainMenu.setSize(500,480);
-        pnlMainMenu.setBackground(colorBlue);
-        pnlMainMenu.add(lblTotalGame);
-        pnlMainMenu.add(lblTotalStaff);
-        pnlMainMenu.add(lblTotalCustomer);
-        pnlMainMenu.add(lblCompanyName);
+        
+        // Panel settings:
+        panelStats.setLayout(null);
+        panelStats.setVisible(true);
+        panelStats.setSize(500,480);
+        panelStats.setBackground(colorVeryBlue);
+        panelStats.add(lblTotalGame);
+        panelStats.add(lblTotalStaff);
+        panelStats.add(lblTotalCustomer);
+        panelStats.add(lblCompanyName);
+        panelStats.add(lblProjectName);
+        
+        panelAddGame.setLayout(null);
+        panelAddGame.setVisible(false);
+        panelAddGame.setSize(500,480);
+        panelAddGame.setBackground(colorVeryBlue);
+        panelAddGame.add(lblAddGameMenu);
+        
+        panelUpdateGame.setLayout(null);
+        panelUpdateGame.setVisible(false);
+        panelUpdateGame.setSize(500,480);
+        panelUpdateGame.setBackground(colorVeryBlue);
+        panelUpdateGame.add(lblUpdateGameMenu);
+        
+        
+        
+        // Adding to JFrame:
+        super.add(panelStats);
+        super.add(panelAddGame);
+        super.add(panelUpdateGame);
         super.setJMenuBar(menuBar);
-        super.add(pnlMainMenu);
-        
-        
-
         super.setVisible(true);
+        
+        
         
         
         
         itemAddGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // panel to add video game
-           }
-       });
-       
-       
-       
-       
-       
+                // Calling add game panel:
+                getContentPane().removeAll();
+                panelAddGame.setVisible(true);
+                add(panelAddGame);
+                panelAddGame.revalidate();
+                panelAddGame.repaint();
+            }
+        });
+        
+        itemUpdateGame.addActionListener(new ActionListener () {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Calling update game panel:
+                getContentPane().removeAll();
+                panelUpdateGame.setVisible(true);
+                add(panelUpdateGame);
+                panelUpdateGame.revalidate();
+                panelUpdateGame.repaint(); 
+            }
+        });
+        
+        itemStats.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Calling main menu's stats panel:
+                getContentPane().removeAll();
+                panelStats.setVisible(true);
+                add(panelStats);
+                panelStats.revalidate();
+                panelStats.repaint();
+            }
+        });
+        
+        
+        
+        
+        
         
         
         
     }
-        
+       
+    
+    
 }
-   
-      
-     
-    
-    
-    
-
-
-    
-
-
